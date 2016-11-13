d18f510e0d980c6ac7e5b2a8a176b7745cea41a6
zigzga <447587073@qq.com>
2016-11-14 00:03:48 +0800

    webpack+vue2.0

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack= require("webpack");
+
+module.exports={
+    entry:{
+        bundle:[ "./src/app.js"]
+    },
+    output:{
+        path:__dirname,
+        publicPath:"/",
+        filename:"build/[name].js"
+    },
+    module:{
+        loaders:[
+            {test: /\.html$/, loaders: ['html']},
+            {test: /(\.js)$/, loader:["babel"] ,exclude:/node_modules/, 
+             query:{
+                    presets:["es2015"]
+             }
+            }
+        ]
+    },
+    resolve:{
+    },
+    plugins:[
+         new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+}
