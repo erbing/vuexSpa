8675b827ba1b0b2b978d38625cb0dece9930a8c9
zigzga <447587073@qq.com>
2016-11-14 01:26:43 +0800

    webpack  + package

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
+
+const Home = {
+    template: '<div> this is home </div>'
+};
+
+const Comm = {
+    template: '<div> this is comm </div>'
+};
+
+Vue.use(VueRouter);
+
+const router = [
+    { path: '/home', component: Home},
+    { path: '/comm', component: Comm}
+];
+
+
+const app = new Vue({
+  router
+}).$mount('#app')
+
