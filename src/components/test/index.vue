7b850c7dff47709f88e0aa17b91e47400a466c3f
zigzga <447587073@qq.com>
2017-04-17 23:47:40 +0800

    add tcomp test

---
+++ src/components/test/index.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div>
+    
+        <h1>{{text}}</h1>
+
+    </div>
+
+</template>
+
+
+<script>
+
+    export default {
+        name: 'test',
+        props: {
+            text: ''
+        }
+    }
+
+</script>
+
+<style></style>
